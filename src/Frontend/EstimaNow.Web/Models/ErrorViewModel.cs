c6acfad7c376735214c6b0d56911611371bc94f2
jvictorportela <jvictorportela30@outlook.com>
2026-09-06 11:59:04 -0300

    Estrutura criada DDD

---
+++ src/Frontend/EstimaNow.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EstimaNow.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
